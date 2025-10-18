5356fd518871a62b7d1e6016c6332a6d4b96283a
Dmitrii Starikov <dd.starikov@gmail.com>
2025-10-18 13:46:47 +0300

    deploy to VPS

--- scripts/deploy-user.sh
+++ scripts/deploy-user.sh
@@ -6,3 +6,5 @@ sudo -u deploy ssh-keygen -t ed25519 -f /home/deploy/.ssh/id_ed25519 -N ""
 cat /home/deploy/.ssh/id_ed25519.pub >> /home/deploy/.ssh/authorized_keys
 sudo -u deploy cat /home/deploy/.ssh/id_ed25519.pub
 sudo -u deploy cat /home/deploy/.ssh/id_ed25519
+
+git config --global --add safe.directory /var/www/angrytribe
