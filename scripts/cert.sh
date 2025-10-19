cbd97a3279ee5007e0237cc3dacaf52561815934
Dmitrii Starikov <dd.starikov@gmail.com>
2025-10-19 12:26:55 +0300

    configs

--- scripts/cert.sh
+++ scripts/cert.sh
@@ -4,7 +4,7 @@ mkdir -p nginx/.well-known/acme-challenge
 docker compose stop nginx
 
 docker run -it --rm \
-  -v $(pwd)/nginx/letsencrypt:/etc/nginx/letsencrypt \
+  -v $(pwd)/nginx/letsencrypt:/etc/letsencrypt \
   -v $(pwd)/.well-known:/var/www/angrytribe/.well-known \
   certbot/certbot certonly \
   --webroot \
