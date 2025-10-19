656a0b45a4dec5a268b1513c5cb745d17fd31e19
Dmitrii Starikov <dd.starikov@gmail.com>
2025-10-19 13:20:36 +0300

    configs

--- scripts/cert.sh
+++ scripts/cert.sh
@@ -1,8 +1,3 @@
-mkdir -p nginx/ssl
-mkdir -p .well-known/acme-challenge
-
-docker compose up -d nginx
-sleep 2
 docker run -it --rm \
   -v $(pwd)/nginx/letsencrypt:/etc/letsencrypt \
   -v $(pwd)/.well-known:/var/www/angrytribe/.well-known \
@@ -11,5 +6,3 @@ docker run -it --rm \
   --webroot-path /var/www/angrytribe \
   -d angrytribe.org \
   -d www.angrytribe.org
-
-docker compose stop nginx
